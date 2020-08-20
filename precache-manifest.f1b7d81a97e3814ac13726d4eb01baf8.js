c1d2a5a00d48aaec0762fde359d525277253405c
Benjamin Meena <59247187+BMeeNA57@users.noreply.github.com>
2020-08-20 12:36:38 -0600

    Updates

---
+++ precache-manifest.f1b7d81a97e3814ac13726d4eb01baf8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4440332015344b46925aeed4163fd25",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "cdaa1511f30a6b7e9717",
+    "url": "/port/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b20a487ccbfb1f4b00d3",
+    "url": "/port/static/css/main.f512cfeb.chunk.css"
+  },
+  {
+    "revision": "cdaa1511f30a6b7e9717",
+    "url": "/port/static/js/2.1eae2916.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/port/static/js/2.1eae2916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20a487ccbfb1f4b00d3",
+    "url": "/port/static/js/main.07404625.chunk.js"
+  },
+  {
+    "revision": "753af5bc66537a30481a",
+    "url": "/port/static/js/runtime-main.3bdbde5f.js"
+  },
+  {
+    "revision": "5e520958f0a48bbf8dad9a091d58f3eb",
+    "url": "/port/static/media/ben-2.5e520958.jpg"
+  },
+  {
+    "revision": "5748f66613de9ac009fd9337e99b6e6b",
+    "url": "/port/static/media/pizza.5748f666.jpg"
+  },
+  {
+    "revision": "8620a31d70d908a81d1db6a70c939fe7",
+    "url": "/port/static/media/weather.8620a31d.jpg"
+  }
+]);
